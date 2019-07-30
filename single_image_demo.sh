904f7ef59922a383f8df27b0f6a57462e270b00d
Yuliang Liu <34134635+Yuliang-Liu@users.noreply.github.com>
2019-07-30 23:28:42 +0930

    Update single_image_demo.sh

--- single_image_demo.sh
+++ single_image_demo.sh
@@ -1,5 +1,5 @@
 DIR=/path/to/JPG_IMAGE_DIR
-ODIR=/path/to/OUTPUT_DIR
+ODIR=/path/to/OUTPUT_DIR/
 CUDA_VISIBLE_DEVICES=0 python demo/test_single_image.py \
 	--min-image-size 1000 \
 	--config-file configs/r50_baseline.yaml \
